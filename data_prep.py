f6e9d717d501e81bbddfaf71dbd36e77c2f632a5
sh-mukherjee <71715720+sh-mukherjee@users.noreply.github.com>
2024-06-26 18:52:16 +0800

    Create data_prep.py
    Module for fetching and preparing the data

---
+++ data_prep.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import country_converter as coco
+from pathlib import Path
+
+file_path = Path(__file__).parent / "electricity-generation_country_emissions.csv"
+df = pd.read_csv(file_path).query('gas == "co2"')
+
+# Convert the 'end_time' column to datetime
+# Extract the year and create a new column
+
+df['year'] = pd.to_datetime(df['end_time']).dt.strftime('%Y')
+
+# extract the year from the 'end_time' column
+# df['Year'] = df['end_time'].str[:4]
+
+# convert the ISO3 country column to a list and passing it to the convert method to add the short name of the country
+df['country'] = coco.convert(names=df.iso3_country.tolist(), to='name_short', not_found=None)
+
+countries = df.country.unique().tolist()
+years = df.year.unique().tolist()
+
+
