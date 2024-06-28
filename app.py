5afb7e8b6b26cc588ba07334f31d6214bc736d44
sh-mukherjee <71715720+sh-mukherjee@users.noreply.github.com>
2024-06-28 17:27:47 +0800

    Updated data and code

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import plotly.express as px
 from shiny.express import input, ui
+from shiny import reactive
 from shinywidgets import render_plotly
 
-from data_prep import df, countries, years
-ui.h1("Electricity Generation Emissions Dashboard")
+from data_prep import df, countries, years, gases
+ui.h1("Transportation Emissions Dashboard")
+ui.p("Transportation Emissions Data by Country from climatetrace.org")
 
 with ui.sidebar():
-    ui.input_select('country', 'Select Country', countries)
-    ui.input_slider('year', 'Select Year', years[0], years[-1],2022)
+    ui.input_selectize('country', 'Select Country', countries, multiple=True)
+    #ui.input_slider('year', 'Select Year', years[0], years[-1],2022)
+    ui.input_select('gas', 'Select Gas', gases)
 
+@reactive.calc
+def filter_data():
+    mask = (df.country.isin(input.country())) & (df.gas == input.gas())
+    return df[mask]
 
 @render_plotly
 def plot1():
-    return px.bar(df[df.country == input.country()], x="year", y="emissions_quantity")
+    return px.bar(filter_data(), 
+                  x="year", 
+                  y="emissions_quantity", 
+                  color="country",
+                barmode='group')
--- data_prep.py
+++ data_prep.py
@@ -2,22 +2,22 @@ import pandas as pd
 import country_converter as coco
 from pathlib import Path
 
-file_path = Path(__file__).parent / "electricity-generation_country_emissions.csv"
-df = pd.read_csv(file_path).query('gas == "co2"')
+file_path = Path(__file__).parent / "road-transportation_country_emissions.csv"
+
+df = pd.read_csv(file_path)
 
 # Convert the 'end_time' column to datetime
 # Extract the year and create a new column
 
 df['year'] = pd.to_datetime(df['end_time']).dt.strftime('%Y')
 
-# extract the year from the 'end_time' column
-# df['Year'] = df['end_time'].str[:4]
 
 # convert the ISO3 country column to a list and passing it to the convert method to add the short name of the country
 df['country'] = coco.convert(names=df.iso3_country.tolist(), to='name_short', not_found=None)
 
 countries = df.country.unique().tolist()
+gases = df.gas.unique().tolist()
 years = df.year.unique().tolist()
 # Convert the list of characters to a list of integers
-years = [int(year) for year in years]
+#years = [int(year) for year in years]
 
